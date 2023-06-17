a486c0f09a130cb491b8af843dcf34fc1e69e7e3
DaianaEscudero <112595487+DaianaEscudero@users.noreply.github.com>
2023-06-17 20:44:55 -0300

    Update Conexion.py
    Agrego puntos 1-2 clase 10

--- Python/Leccion07/Conexion.py
+++ Python/Leccion07/Conexion.py
@@ -1,8 +1,9 @@
-import logging
 
-import psycopg2 as bd
-from logger_base import log
-import sys
+
+from psycopg2 import pool
+#psycopg2 as db otra manera de importar el psycopg2
+#from logger_base import log
+#import sys
 
 
 class Conexion:
@@ -11,44 +12,27 @@ class Conexion:
     _PASSWORD = "admin"
     _DB_PORT = "5432"
     _HOST = "127.0.0.1"
-    _conexion = None
-    _cursor = None
+    _MIN_CON = 1
+    _MAX_CON = 5
+    _pool = None 
 
     @classmethod
     def obtenerConexion(cls):
-        if cls._conexion is None:
-            try:
-                cls._conexion = bd.connect(host=cls._HOST,
-                                           user=cls._USERNAME,
-                                           password=cls._PASSWORD,
-                                           port=cls._DB_PORT,
-                                           database=cls._DATABASE)
-                logging.debug(f"Conexión Exitosa: {cls._conexion}")
-                return cls._conexion
-            except Exception as e:
-                log.error(f"Ocurrió un error: {e}")
-                sys.exit()
-        else:
-            return cls._conexion
+        pass 
 
     @classmethod
     def obtenerCursor(cls):
-        if cls._cursor is None:
+        pass
+
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None: 
             try:
-                cls._cursor = cls.obtenerConexion().cursor()
-                log.debug(f"Se abrió correctamente el cursor: {cls._cursor}")
-                return cls._cursor
-            except Exception as e:
-                log.error(f"Ocurrió un error: {e}")
-                sys.exit()
-        else:
-            return cls._cursor
+                cls._pool = pool.SimpleConnectionPool()
+            
 
 if __name__ == "__main__":
-    Conexion.obtenerConexion()
-    Conexion.obtenerCursor()
-
-
+   pass
 
 
 
